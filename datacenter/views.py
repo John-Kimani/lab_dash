0ae5364894ac867ffe2c3cf8397787fe4d133b63
John-Kimani <kimwaweru100@gmail.com>
2022-10-22 17:56:00 +0300

    Dev: Create view templates

--- datacenter/views.py
+++ datacenter/views.py
@@ -2,8 +2,24 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 
+datas = [
+    {
+        'agent_id': 100, 
+        'agent_name': 'Kimani', 
+        "location": "Nairobi"
+    },
+    {
+        'agent_id': 101, 
+        'agent_name': 'John', 
+        "location": "Nairobi"
+    },
+]
+
 def homePage(request):
     '''
     Function to view homepage
     '''
-    return HttpResponse('<h1>Home Page Works</h1>')
+    context = {
+        "datas": datas
+    }
+    return render(request, 'datacenter/home.html', context)
