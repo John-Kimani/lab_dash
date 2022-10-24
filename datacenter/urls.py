0a1fe90b8ebc83ef887dc2b254ff29d310739842
John-Kimani <kimwaweru100@gmail.com>
2022-10-24 15:49:17 +0300

    Dev: User registration form

--- datacenter/urls.py
+++ datacenter/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.homePage, name='homePage'),
+    path('register', views.signUp, name='register')
 ]
--- datacenter/views.py
+++ datacenter/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from .forms import UserRegistrationForm
+from django.contrib import messages
 # Create your views here.
 
 datas = [
@@ -23,3 +24,17 @@ def homePage(request):
         "datas": datas
     }
     return render(request, 'datacenter/home.html', context)
+
+
+def signUp(request):
+    if request.method == 'POST':
+        form = UserRegistrationForm(request.POST)
+
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            form.save()
+            messages.success(request, f'Your account has been created. Please login')
+            return redirect('loginPage')
+    else:
+        form = UserRegistrationForm()
+    return render(request, 'datacenter/register.html' ,{'form': form})
